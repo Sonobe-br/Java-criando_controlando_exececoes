e184882a429851dbf404499685edc6238c44d480
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-03-02 12:26:57 -0300

    StackOverFlowError

---
+++ FluxoComError.java
@@ -0,0 +1,35 @@
+
+public class Fluxo {
+	
+	 public static void main(String[] args) {
+	        System.out.println("Ini do main");
+	     
+	        try {
+	        	
+	        	metodo1();    	
+	        	
+	        } catch (ArithmeticException | NullPointerException ex) {
+                String msg = ex.getMessage();
+	        	System.out.print ("Exception" + msg);
+	        	ex.printStackTrace();
+	        	
+	        } 
+	        System.out.println("Fim do main");
+	    }
+
+	    private static void metodo1() {
+            System.out.println("Ini do metodo1");
+
+	        metodo2();
+	        System.out.println("Fim do metodo1");
+	    
+	    }
+
+	    private static void metodo2() {
+            System.out.println("Chamando o metodo2");
+	       
+	        metodo2();
+	        System.out.println("Entregando o metodo2");
+	    }
+
+}
